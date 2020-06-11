003f2a8e9f82fa5641b738d5c926033192496e75
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-06-11 19:59:27 -0400

    text input dialogs

--- src/script/app.js
+++ src/script/app.js
@@ -3,7 +3,7 @@ Electron = require('electron');
 
 $(document).ready(function() {
   $("#buttonAddFiles").click(function() {
-    Electron.remote.dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }).then((data) => {
+    Electron.remote.dialog.showOpenDialog({ filters: [{ name: 'XML', extensions: [ 'xml' ] }], properties: ['openFile', 'multiSelections'] }).then((data) => {
       data.filePaths.forEach( e => {
         $("#filelist").append(function() {
           return $('<listelem data-path="' + e + '">' + e.replace(/^.*[\\\/]/, '') + '</listelem>').click(function() {
@@ -18,6 +18,18 @@ $(document).ready(function() {
     [].slice.call($("#filelist").children()).forEach( e => {
       if (e.classList.contains("selected"))
         e.remove();
+    });
+  });
+
+  $("#textPath").click(function() {
+    Electron.remote.dialog.showOpenDialog({ properties: [ 'openDirectory' ] }).then((data) => {
+      $(this).val(data.filePaths[0]);
+    });
+  });
+
+  $("#textTemplate").click(function() {
+    Electron.remote.dialog.showOpenDialog({ filters: [{ name: 'HTML', extensions: [ 'htm', 'html' ] }] }).then((data) => {
+      $(this).val(data.filePaths[0]);
     });
   });
 });
