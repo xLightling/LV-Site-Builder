7a26f9036087e4cc3882e27eac1db37d4e88d566
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-06-11 19:41:36 -0400

    of course there's an indent issue and missing semicolons with this endless Peak JavaScript(TM) chain good lord

--- src/script/app.js
+++ src/script/app.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
   $("#buttonAddFiles").click(function() {
     Electron.remote.dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }).then((data) => {
       data.filePaths.forEach( e => {
-          $("#filelist").append(function() {
-            return $('<listelem data-path="' + e + '">' + e.replace(/^.*[\\\/]/, '') + '</listelem>').click(function() {
-              $(this).toggleClass("selected");
-          })
-        })
+        $("#filelist").append(function() {
+          return $('<listelem data-path="' + e + '">' + e.replace(/^.*[\\\/]/, '') + '</listelem>').click(function() {
+            $(this).toggleClass("selected");
+          });
+        });
       });
     });
   });
