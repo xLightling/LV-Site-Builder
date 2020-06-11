d536bb38f43a17b463ec312f250f1dd47ea17e1c
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-06-11 19:23:43 -0400

    list element selection

--- src/script/app.js
+++ src/script/app.js
@@ -4,7 +4,13 @@ Electron = require('electron');
 $(document).ready(function() {
   $("#buttonAddFiles").click(function() {
     Electron.remote.dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }).then((data) => {
-      data.filePaths.forEach( e => { $("#filelist").append('<listelem data-path="' + e + '">' + e.replace(/^.*[\\\/]/, '') + '</listelem>'); });
+      data.filePaths.forEach( e => {
+          $("#filelist").append(function() {
+            return $('<listelem data-path="' + e + '">' + e.replace(/^.*[\\\/]/, '') + '</listelem>').click(function() {
+              $(this).toggleClass("selected");
+          })
+        })
+      });
     });
-  });
+  })
 });
